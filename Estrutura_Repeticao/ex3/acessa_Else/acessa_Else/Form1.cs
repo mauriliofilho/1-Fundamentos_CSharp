eab5a747b3d18f10fc9a696e054577a5c490b2f2
Maurilio Filho <mauriliofilho22@gmail.com>
2014-11-17 18:15:25 +0000

    Acessando a opçao elso
    Isso ocorre quando a opçao if nao satisfaz a condição de verificação.

---
+++ Estrutura_Repeticao/ex3/acessa_Else/acessa_Else/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace acessa_Else
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double saldo = 5.0;
+            double valorSaque = 10.0;
+            if (saldo >= valorSaque)
+            {
+                saldo -= valorSaque;
+                MessageBox.Show("Saque Realizado com sucesso. " + "Saldo é " + saldo);
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente. " + "O Saldo é: " + saldo);
+            }
+        }
+    }
+}
