dc426401ce4b065e6a4c302cf6998a9b99d95d83
Maurilio Filho <mauriliofilho22@gmail.com>
2014-11-17 16:55:48 +0000

    Exemplos de media e conversao

---
+++ ex3/mediaIdades/mediaIdades/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mediaIdades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idadeVanessa = 24;
+            double idadeMarissa = 1.8;
+            int idadeMaurilio = 30;
+            double mediaIdade;
+
+            mediaIdade = (idadeMarissa + idadeMaurilio + idadeVanessa) / 3;
+
+            MessageBox.Show("A media de idadews da minha familia é : " + mediaIdade);
+        }
+    }
+}
